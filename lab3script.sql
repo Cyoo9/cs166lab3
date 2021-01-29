022ba6da54bacdb51408e4c74509eb985bf69340
Taeho Yoo <tyoo002@wch133-37.cs.ucr.edu>
2021-01-29 15:28:33 -0800

    finished part 1

--- lab3script.sql
+++ lab3script.sql
@@ -1,26 +1,26 @@
-DROP TABLE Professor;
-DROP TABLE Project;
-DROP TABLE Dept;
-DROP TABLE Graduate;
-DROP TABLE work_dept;
-DROP TABLE work_in
-DROP TABLE advise;
-DROP TABLE work_proj; 
+DROP TABLE Professor CASCADE;
+DROP TABLE Dept CASCADE;
+DROP TABLE work_dept CASCADE;
+DROP TABLE Project CASCADE;
+DROP TABLE work_in CASCADE;
+DROP TABLE Graduate CASCADE;
+DROP TABLE work_proj CASCADE;
+DROP TABLE advise CASCADE;
 
 CREATE TABLE Professor( ssn CHAR(11) NOT NULL, 
 			name CHAR(30) NOT NULL, 
 			age INTEGER NOT NULL, 
 			rank INTEGER NOT NULL, 
 			specialty CHAR(30) NOT NULL, 
-			PRIMARY KEY(ssn)); 
+			PRIMARY KEY(ssn));  
 
 CREATE TABLE Dept ( dno INTEGER, 
 		    dname CHAR(30), 
 		    office CHAR(30), 
 		    ssn CHAR(11) NOT NULL, 
 		    PRIMARY KEY(dno), 
-		    FOREIGN KEY(ssn) REFERENCES Professor(ssn),
-	            ON DELETE NO ACTION ); 
+		    FOREIGN KEY(ssn) REFERENCES Professor(ssn)
+	            ); 
 
 CREATE TABLE work_dept ( time_pc CHAR(20) NOT NULL, 
 			 ssn CHAR(11) NOT NULL, 
@@ -37,8 +37,7 @@ CREATE TABLE Project (  pno INTEGER,
 			budget INTEGER,
 			ssn CHAR(11) NOT NULL, 
 			PRIMARY KEY(pno), 
-			FOREIGN KEY(ssn) REFERENCES Professor(ssn),
-			ON DELETE NO ACTION ); 
+			FOREIGN KEY(ssn) REFERENCES Professor(ssn)); 
 
 CREATE TABLE work_in(   ssn CHAR(11) NOT NULL, 
 			pno INTEGER NOT NULL, 
@@ -53,8 +52,8 @@ CREATE TABLE Graduate(SSN CHAR(11),
 			age INTEGER, 
 			deg_pg CHAR(20), 
 			PRIMARY KEY(SSN), 
-			FOREIGN KEY (dno) REFERENCES Dept(dno), 
-			ON DELETE NO ACTION); 
+			FOREIGN KEY (dno) REFERENCES Dept(dno) 
+			); 
 
 CREATE TABLE work_proj ( pno INTEGER NOT NULL, 
 		 	 SSN CHAR(11) NOT NULL, 
@@ -65,3 +64,5 @@ CREATE TABLE work_proj ( pno INTEGER NOT NULL,
 CREATE TABLE advise (SSN CHAR(11) NOT NULL, 
 			PRIMARY KEY(SSN),
 			FOREIGN KEY(SSN) REFERENCES Graduate(SSN));  
+
+
